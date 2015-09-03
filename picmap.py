c83c30adf66b0285b24944058755e09632345a91
unknown <samt@samt.st>
2015-09-03 20:30:00 +0200

    more or less working now :)

--- picmap.py
+++ picmap.py
@@ -8,6 +8,9 @@ import sqlite3
 from get_lat_lon_exif_pil import *
 import get_lat_lon_exif_pil
 
+import mapnik
+import simplekml
+
 def get_files(s_base_dir):
 	fileList = []
 
@@ -27,6 +30,13 @@ def get_db_files(db_cursor):
 
 	return fileList
 
+def get_db_file_rows(db_cursor):
+	db_cursor.execute('''SELECT lat, lon FROM files''')
+
+	data=db_cursor.fetchall()
+
+	return data
+
 def process_new_file(s_path):
 	# get geo
 	o_image = Image.open(s_path)
@@ -38,24 +48,6 @@ def process_new_file(s_path):
 
 	lat = lat_lon[0]
 	lon = lat_lon[1]
-
-	"""
-	try:
-		lat = lat_lon[0]
-		lon = lat_lon[1]
-	except:
-		lat = 1000
-	else:
-		lat = 1000
-	"""
-
-	"""
-	if lat_lon[0] not None:
-		lat = lat_lon[0]
-
-	if lat_lon[1] not None:
-		lon = lat_lon[1]
-	"""
 
 	# add to files table
 	cursor.execute('''INSERT OR IGNORE INTO files(file,lat,lon, hasgeo) VALUES(?,?,?,?)''', (s_path, lat, lon, 1,))
@@ -86,10 +78,59 @@ if __name__ == '__main__':
 			# we found it
 			i_added += 1
 
-	print ("added %i files", (i_added))
+	print "added %i files" % (i_added)
 
-
-
-	# persist and closee db
+	# persist 
 	db.commit()
+
+	# render
+	db_files = get_db_file_rows(cursor)
+
+	#closee db
 	db.close()
+
+	print "%i files found to render" % (len(db_files))
+
+
+	kml = simplekml.Kml()
+
+	for r_file in db_files:
+		pnt = kml.newpoint()
+		#pnt.coords = [(r_file[0], r_file[1])]
+		pnt.coords = [(r_file[1], r_file[0])]
+		pnt.style.labelstyle.color = simplekml.Color.red  # Make the text red
+		pnt.style.labelstyle.scale = 2  # Make the text twice as big
+		pnt.style.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png'
+
+
+	kml.save('kml_geo.kml')
+
+
+
+	width_in_px = 1000
+	height_in_px = 1000
+	mapfile = 'mapnik_style.xml'
+
+	map_canvas = mapnik.Map(width_in_px, height_in_px)
+	mapnik.load_map(map_canvas, mapfile)
+	map_canvas.background = mapnik.Color('rgb(0,0,0,0)') # transparent
+
+	# Create a symbolizer to draw the points
+	style = mapnik.Style()
+	rule = mapnik.Rule()
+	point_symbolizer = mapnik.MarkersSymbolizer()
+	point_symbolizer.allow_overlap = True
+	point_symbolizer.opacity = 0.5 # semi-transparent
+	rule.symbols.append(point_symbolizer)
+	style.rules.append(rule)
+	map_canvas.append_style('GPS_tracking_points', style)
+
+	# Create a layer to hold the ponts
+	layer = mapnik.Layer('GPS_tracking_points')
+	layer.datasource = mapnik.Ogr(file="kml_geo.kml", layer_by_index=0)
+	layer.styles.append('GPS_tracking_points')
+	map_canvas.layers.append(layer)
+
+	# Save the map
+	map_canvas.zoom_all()
+	mapnik.render_to_file(map_canvas, 'GPS_tracking_points.png', 'png')
